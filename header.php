9b02a77b12cd376ddd1c7fd0042ab2e6d4fc0dfa
Chupryna-IV <chupryna.ihor@gmail.com>
2023-03-13 12:41:22 +0200

    add new page commit2

--- header.php
+++ header.php
@@ -5,7 +5,8 @@ $header = $config["header"];
 echo "<ul>\n";
 foreach ($header as $menu) {
     foreach ($menu as $val) {
-        echo '<a style="margin-right: 5px" href="' . $val['link'] . '">' . $val['title'] . '</a>';
+
+        echo '<li><a style="margin-right: 5px" href="' . $val['link'] . '">' . $val['title'] . '</a></li>';
     }
 }
 echo "</ul>\n";
